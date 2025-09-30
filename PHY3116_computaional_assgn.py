c01895ea44a8255064846f16f2a066a3b2a3dfb4
kenji700 <z5422303@ad.unsw.edu.au>
2025-09-30 15:49:56 +1000

    Create PHY3116_computaional_assgn.py
    This is a skeleton of a program.  Hopefully we can modify or add codes to this file.

---
+++ PHY3116_computaional_assgn.py
@@ -0,0 +1,18 @@
+# This is a skeleton program for the computational assignment of PHYS3116
+# As of 30 Sep we haven't decided which option we will undertake.
+# function declaration
+# These function declarations are just for the initial starting stubs.
+# we may not necessaryly split our code contributions into these functions.
+def  student_1():
+    return
+def student_2():
+    return
+def student_3():
+    return
+def read_data():
+    f = open("demofile.txt")
+    close(f)
+    return
+def starter():
+    #open data file
+    read_data()
