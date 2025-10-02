23a8efbee91dbdc2b52501707da43d6ccfa4b752
kelvin chen <kelvin.chen342@gmail.com>
2025-10-02 21:37:14 +1000

    Graphing of FeH vs Age

--- import_numpy_as_np.py
+++ import_numpy_as_np.py
@@ -1,18 +1,15 @@
 import numpy as np
-stellar_data = np.loadtxt('/Users/kelvinchen/Downloads/HarrisPartI.csv', delimiter=',', skiprows=1, dtype=object)
-print(stellar_data)
-ID = stellar_data[:,0]
-Name=stellar_data[:,1]
-RA=stellar_data[:,2]
-Dec=stellar_data[:,3]
-L=stellar_data[:,4]
-B=stellar_data[:,5]
-R_Sun=stellar_data[:,6]
-R_gc=stellar_data[:,7]
-X=stellar_data[:,8]
-Y=stellar_data[:,9]
-Z=stellar_data[:,10]
-print(X)
-
-
+import matplotlib.pyplot as plt
+import matplotlib.colors as colours
+import cmasher as cm
+stellar_data = np.loadtxt('Krause21.csv', delimiter=',', skiprows=1, dtype=object)
+Age = stellar_data[:,6]
+FeH = stellar_data[:,7]
+cmap = colours.ListedColormap(['blue', 'green', 'red'])
+cmap_2 = cm.tropical
+plt.scatter(Age.astype(float), FeH.astype(float))
+plt.xlabel('Age')
+plt.ylabel('FeH')
+plt.title('Age vs FeH')
+plt.show()
 
