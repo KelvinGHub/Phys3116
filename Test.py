78d823f28684dcdc2755a6b6aa1897c2c430f12c
kelvin chen <kelvin.chen342@gmail.com>
2025-10-17 15:20:57 +1100

    commit

--- Test.py
+++ Test.py
@@ -9,6 +9,3 @@ from astropy.io import ascii
 vandenberg = ascii.read("vandenBerg_table2.csv", format='basic', delimiter=',', guess=False)
 krause21 = ascii.read("Krause21.csv", format='basic', delimiter=',', guess=False)
 combined = ascii.join([vandenberg, krause21], keys=['Cluster name'], join_type='inner')
-# Instead of print(joined.colnames)
-for name in combined.colnames:
-    print(repr(name))
