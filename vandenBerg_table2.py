ae589798dfa26058f83ca597c7750a3c4ca2fdb3
KevinPan03 <kaiwenpan2003@gmail.com>
2025-10-23 14:48:44 +1100

    Some simple changes (adding linear regression lines)

--- vandenBerg_table2.py
+++ vandenBerg_table2.py
@@ -10,8 +10,18 @@ data = pd.read_csv('vandenBerg_table2.csv')
 data = data[(data['Age'] > 0) & (data['FeH'] > -99)]
 
 #plotting the data
-plt.scatter(data['Age'], data['FeH'], marker='.')
-cbar = plt.colorbar()
+plt.scatter(np.log10(data['Age']), data['FeH'], marker='.')
+
+# linear regression
+x_log=np.log10(data['Age'])
+slope, intercept, r_value, p_value, std_err = stats.linregress(x_log, data['FeH'])
+print(f"Slope: {slope}, Intercept: {intercept}, R-squared: {r_value**2}")
+
+# Plotting the regression line
+x_fit = np.linspace(min(x_log), max(x_log), 100)
+y_fit = intercept + slope * x_fit
+plt.plot(x_fit, y_fit, color='red', label='Linear Fit')
+
 
 # Labeling the plot
 plt.title(r'Metallicity vs Age for Globular Clusters')
